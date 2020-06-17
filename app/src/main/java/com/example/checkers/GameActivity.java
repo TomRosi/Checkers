d0114d6a6931e9342717361190eeb8c3910687ec
Tomas Rozsypal <Tomas.Rozsypal@aimtecglobal.com>
2020-06-17 13:31:52 +0200

    Začátek

---
+++ app/src/main/java/com/example/checkers/GameActivity.java
@@ -0,0 +1,12 @@
+package com.example.checkers;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+    }
+}
